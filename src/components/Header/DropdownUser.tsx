0935fb1573d9150055ea0c3997ee79dccdef3bd5
André Lademann <alademann@thinkport.digital>
2024-04-03 00:41:45 +0200

    update: update user name in DropdownUser component from "Thomas Anree" to "Thomas Mertens"

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -44,7 +44,7 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            Thomas Anree
+            Thomas Mertens
           </span>
           <span className="block text-xs">Retail Manager</span>
         </span>
