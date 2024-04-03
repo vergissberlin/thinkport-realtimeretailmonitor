32dc734153130142ea67a7e08dc9d9a1a93166af
André Lademann <alademann@thinkport.digital>
2024-04-03 20:22:20 +0200

    feat: Add d3 map chart

--- src/components/Tables/TableOne.tsx
+++ src/components/Tables/TableOne.tsx
@@ -48,7 +48,7 @@ const TableOne = () => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        Top Channels
+        Top Channelss
       </h4>
 
       <div className="flex flex-col">
