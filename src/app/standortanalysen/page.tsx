ecc2aa56eda9b18c472fc9b5154b0d1f6668e505
André Lademann <alademann@thinkport.digital>
2024-04-03 14:16:38 +0200

    ✨ feat: Add new pages for 'Bestandsbewegung' and 'Standortanalysen'
    This commit introduces new files 'page.tsx' for 'Bestandsbewegung' and 'Standortanalysen', including the page components with metadata, layout, breadcrumbs, and content related to performance comparison with competitors in terms of prices, product offerings, and sales trends.

---
+++ src/app/standortanalysen/page.tsx
@@ -0,0 +1,30 @@
+import Chart from "@/components/Charts/page";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import React from "react";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+
+export const metadata: Metadata = {
+  title: "Bestandsbewegung",
+  description:
+    "This is Next.js Chart page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+};
+
+const BestandsbewegungPage: React.FC = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Bestandsbewegung" />
+
+      <div className="rounded-sm border border-stroke bg-white p-4 shadow-default dark:border-strokedark dark:bg-boxdark md:p-6 xl:p-9">
+        <div className="flex flex-col gap-7.5">
+          <p>
+            Vergleich der eigenen Leistung mit der der Wettbewerber,
+            einschließlich Preise, Produktangebot und Verkaufstrends.
+          </p>
+        </div>
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default BestandsbewegungPage;
