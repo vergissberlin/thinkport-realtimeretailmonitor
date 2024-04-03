8728f78b8fb37c58ec00940b24246cf41f4d1fa2
André Lademann <alademann@thinkport.digital>
2024-04-03 21:01:24 +0200

    🚑 fix: update assetPrefix to use NEXT_ASSET_PREFIX instead of NEXT_PUBLIC_BASE_PATH

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '', 
+    assetPrefix: process.env.NEXT_ASSET_PREFIX || '', 
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
     images: {
         unoptimized: true,
